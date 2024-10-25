18e9de9c94e594949c753b2bb67b0fe9d6e0e92b
homermm <monsalvohomero@gmail.com>
2024-10-25 18:09:25 -0300

    FEAT: SearchPurchaseOrders with filters added

---
+++ Services/IPurchaseOrderService.cs
@@ -0,0 +1,7 @@
+﻿namespace soap_client.Services
+{
+    public interface IPurchaseOrderService
+    {
+        Task<List<PurchaseOrderSoap>> SearchPurchaseOrdersAsync(string? productCode = null, DateTime? startRequestDate = null, DateTime? endRequestDate = null, string? status = null, string? storeCode = null);
+    }
+}
---
+++ Services/Implementations/PurchaseOrderService.cs
@@ -0,0 +1,41 @@
+﻿namespace soap_client.Services.Implementations
+{
+    public class PurchaseOrderService : IPurchaseOrderService
+    {
+        private readonly PurchaseOrderPortClient _client;
+
+        public PurchaseOrderService(PurchaseOrderPortClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<PurchaseOrderSoap>> SearchPurchaseOrdersAsync(
+            string? productCode = null, 
+            DateTime? startRequestDate = null, 
+            DateTime? endRequestDate = null, 
+            string? status = null, 
+            string? storeCode = null)
+        {
+
+            var request = new SearchPurchaseOrdersRequest
+            {
+                productCode = string.IsNullOrEmpty(productCode) ? null : productCode,
+                startRequestDate = startRequestDate,
+                endRequestDate = endRequestDate,
+                status = string.IsNullOrEmpty(status) ? null : status,
+                storeCode = string.IsNullOrEmpty(storeCode) ? null : storeCode
+            };
+
+            var response = await _client.SearchPurchaseOrdersAsync(request);
+
+            return response.SearchPurchaseOrdersResponse1?.ToList() ?? new List<PurchaseOrderSoap>();
+  
+        }
+    }
+    }
+
+
+
+
+
+
